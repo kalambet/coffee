5baa7d2a0a932e1938f7f8f8a528ed4be11c4c96
Petr Kalambet <peter@technogeek.io>
2015-02-04 22:30:50 +0300

    Folder structure reorganization

---
+++ FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Coffee4Fun
+//
+//  Created by Petr Kalambet on 30/01/15.
+//  Copyright (c) 2015 Petr Kalambet. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
